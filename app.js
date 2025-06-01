8fffc1f89a65b2808a8d18a2d9162c57e655567d
tqthai2812 <tqthaimax@gmail.com>
2025-06-01 22:16:52 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcom to contact book application."});
